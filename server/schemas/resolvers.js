12bc2c8f113e030adf4447e464c1a92f735b135b
BillStephens2022 <two4onebill@yahoo.com>
2023-02-06 16:59:42 -0500

    added LOGIN_USER, ADD_USER, and SAVE_BOOK mutations on client side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const resolvers = {
     // me: User
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate("thoughts");
+        return User.findOne({ _id: context.user._id });
       }
       throw new AuthenticationError("You need to be logged in!");
     },
