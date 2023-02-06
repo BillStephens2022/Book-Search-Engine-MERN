54a890246e80252c7ae7951d77f12a62b558bf0b
BillStephens2022 <two4onebill@yahoo.com>
2023-02-06 15:34:16 -0500

    updated server.js with apollo/graphql boilerplate code, started to update typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    title: String!
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
