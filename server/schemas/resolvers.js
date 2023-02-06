90b33318b0c996d0e63a1cd4bd78f413ccebf9d3
BillStephens2022 <two4onebill@yahoo.com>
2023-02-06 16:28:15 -0500

    updated resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,17 +1,69 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        // me: User
+  Query: {
+    // me: User
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("thoughts");
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
-    Mutation: {
-        // addUser
-        // login
-        // saveBook
-        // removeBook
-    }
-}
+  },
+  Mutation: {
+    // addUser
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    // login
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-module.exports= resolvers;
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // saveBook
+    saveBook: async (parent, { bookId, authors, description, image, link, title }, context) => {
+        if (context.user) {
+            return User.findOneAndUpdate(
+                { _id: context.user._id },
+                {
+                    $addToSet: {
+                        savedBooks: { bookId, authors, description, image, link, title },
+                    },
+                },
+                { new: true, runValidators: true }
+            );
+        } 
+        throw new AuthenticationError("You need to be logged in!");
+    },
+    // removeBook
+    removeBook: async (parent, { bookId }, context ) => {
+        if (context.user) {
+            return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true, runValidators: true }
+            );
+        }
+        throw new AuthenticationError("You need to be logged in!");
+    }
+  }
+};
+
+module.exports = resolvers;
